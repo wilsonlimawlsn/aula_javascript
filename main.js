1734229fc36eefba6810aba36084b9f1fb9292ac
wilson <wilsonlimawlsn@gmail.com>
2020-11-10 17:25:07 -0300

    introdução ao javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var nome = "Wilson Lima";
+var idade = 32;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+//alert(nome + " tem " + idade + " anos.");
+console.log(nome);
+console.log(idade2 + idade);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(idade + idade2);
+//alert(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
